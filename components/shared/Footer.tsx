00e74e62f1d5b2ef81df4159cd01752aa50cfe3d
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-12-21 01:17:41 +0000

    Responsive footer added

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../public/assets/icons/Evanta-logo.png";
+
+const year = new Date().getFullYear();
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="wrapper flex justify-between items-center max-md:flex-col max-md:gap-4 text-center p-5">
+        <Link href={"/"} className="w-36 flex items-center gap-1">
+          <Image src={logo} alt="logo" width={38} height={38} />
+          <h1 className={`text-2xl font-[600] font-urbanist`}>Evanta</h1>
+        </Link>
+        <p className="text-sm font-light">{year} Evanta. All rights reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
