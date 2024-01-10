283387231ff4a7dc2ac591646488414371e16e67
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2024-01-10 23:38:25 +0000

    Update page.tsx

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -9,7 +9,7 @@ const CreateEvent = () => {
 
   return (
     <>
-      <section className="bg-prary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <h3 className="h3-bold wrapper text-center sm:text-left">
           Create Event
         </h3>
